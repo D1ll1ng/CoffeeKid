a80504af9e4fb6e7439ed8582b1a9346e1d3fbfc
Magnus Dilling <magnusdilling@gmail.com>
2023-10-02 00:50:25 +0200

    Camera - Made the camera a bit more lower and further away and rotated it to be straight in comparison to the map & and added coffee into the cup

--- scripts/view.gd
+++ scripts/view.gd
@@ -5,7 +5,7 @@ extends Node3D
 
 @export_group("Zoom")
 @export var zoom_minimum = 16
-@export var zoom_maximum = 4
+@export var zoom_maximum = 70
 @export var zoom_speed = 10
 
 @export_group("Rotation")
@@ -18,7 +18,7 @@ var zoom = 10
 
 func _ready():
 	
-	camera_rotation = rotation_degrees # Initial rotation
+	camera_rotation = rotation_degrees * 0 # Initial rotation
 	
 	pass
 
@@ -26,10 +26,10 @@ func _physics_process(delta):
 	
 	# Set position and rotation to targets
 	
-	self.position = self.position.lerp(target.position, delta * 4)
-	rotation_degrees = rotation_degrees.lerp(camera_rotation, delta * 6)
+	self.position = self.position.lerp(target.position, delta * 5)
+	rotation_degrees = rotation_degrees.lerp(camera_rotation, delta * 5)
 	
-	camera.position = camera.position.lerp(Vector3(0, 0, zoom), 8 * delta)
+	camera.position = camera.position.lerp(Vector3(0, 0, zoom), 5 * delta)
 	
 	handle_input(delta)
 
@@ -38,14 +38,13 @@ func _physics_process(delta):
 func handle_input(delta):
 	
 	# Rotation
-	
 	var input := Vector3.ZERO
 	
 	input.y = Input.get_axis("camera_left", "camera_right")
 	input.x = Input.get_axis("camera_up", "camera_down")
 	
 	camera_rotation += input.limit_length(1.0) * rotation_speed * delta
-	camera_rotation.x = clamp(camera_rotation.x, -80, -10)
+	camera_rotation.x = clamp(camera_rotation.x, -80, -3)
 	
 	# Zooming
 	
